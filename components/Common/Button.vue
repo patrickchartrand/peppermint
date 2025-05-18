e479ae671efceeeb922ea977f81fbbd25f3cb4f0
Patrick Chartrand <pchartrand@outlook.com>
2025-05-17 22:19:02 -0400

    refactor: make some changes

--- components/Common/Button.vue
+++ components/Common/Button.vue
@@ -3,7 +3,7 @@ defineProps(['isDark', 'type'])
 </script>
 
 <template>
-    <button :type="type" :class="{ 'prose-light-bright hover:!text-stone-900 bg-stone-800 focus:ring-teal-300 hover:bg-teal-200': isDark, 'prose-dark-bright !text-stone-900 bg-teal-200 focus:ring-stone-300 hover:bg-teal-300': !isDark }" class="uppercase text-xl px-9 pb-4 pt-5 rounded-md hover:px-11 cursor-pointer focus:ring-4 transform-all duration-300">
+    <button :type="type" :class="{ 'prose-light-bright hover:!text-stone-900 bg-stone-800 focus:ring-teal-300 hover:bg-teal-200': isDark, 'prose-dark-bright !text-stone-900 bg-teal-200 focus:ring-stone-300 hover:bg-teal-500': !isDark }" class="uppercase flex justify-center items-center min-h-[3.25rem] text-xl px-9 pt-[0.35rem] rounded-md hover:px-11 cursor-pointer focus:ring-4 transform-all duration-300">
         <slot name="label" />
     </button>
 </template>
